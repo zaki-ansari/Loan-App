ef1806c05ffcd82f7e70a51489f2a8972111e7bc
Zaki Ansari <99637633+zaki-ansari@users.noreply.github.com>
2022-11-09 23:08:26 +0530

    updated signin controller

--- Project-Loan-Application/src/main/java/com/finzly/controller/UserSignInController.java
+++ Project-Loan-Application/src/main/java/com/finzly/controller/UserSignInController.java
@@ -3,6 +3,7 @@ package com.finzly.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.finzly.entity.UserSignIn;
 import com.finzly.service.UserSignInService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/userSignIn")
 public class UserSignInController {
